4daa572929ac43461641da0c8957c43d65f0b3f0
Syedkabir S <gs1-syedkabirs@corp.expeditors.com>
2021-11-25 11:39:21 +0530

    data types and class object

---
+++ week1/src/classTypeObject.java
@@ -0,0 +1,60 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class classTypeObject {
+    public static void main(String[] args) {
+        Student s1 = new Student("Arun", 3213124);
+
+
+        /// accessing class object created by JVM
+
+        Class c1 = s1.getClass();
+
+        System.out.println(c1.getName());
+
+        // can get class's methods into an array
+
+        Method m[] = c1.getDeclaredMethods();
+
+        for(Method method : m){
+            System.out.println(method.getName());
+        }
+
+        // can get class's field
+
+        Field f[] = c1.getDeclaredFields();
+
+        for(Field field : f){
+            System.out.println(field.getName());
+        }
+    }
+
+}
+
+class Student{
+
+    private String name;
+    private int roll_no;
+
+    public Student(String name, int roll_no) {
+        this.name = name;
+        this.roll_no = roll_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll_no() {
+        return roll_no;
+    }
+
+    public void setRoll_no(int roll_no) {
+        this.roll_no = roll_no;
+    }
+}
+
---
+++ week1/src/dataType.java
@@ -0,0 +1,13 @@
+public class dataType {
+
+    // basic data types
+
+    private int myNum = 5;
+    private double myDouble = 989513548.8908897976;
+    private float myFloatNum = 5.99f;
+    private char myLetter = 'D';
+    private boolean myBool = true;
+    private String myText = "Hello";
+
+
+}
