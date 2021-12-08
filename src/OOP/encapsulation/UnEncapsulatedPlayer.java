1219a3e8bb083e32ed9d48f93679430f1c0439c9
Syedkabir S <gs1-syedkabirs@corp.expeditors.com>
2021-12-08 13:55:13 +0530

    OOP Concepts

---
+++ src/OOP/encapsulation/UnEncapsulatedPlayer.java
@@ -0,0 +1,21 @@
+package OOP.encapsulation;
+
+public class UnEncapsulatedPlayer {
+//    Without Encapsulation Fields are public and without validation(less control)
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage){
+        this.health = this.health - damage;
+        if(this.health <=0){
+            System.out.println("Player knocked out"); // reduce player lives
+
+        }
+    }
+
+    public int healthRemaining(){
+
+        return this.health;
+    }
+}
---
+++ src/OOP/interfaces/InterfaceMain.java
@@ -0,0 +1,30 @@
+package OOP.interfaces;
+
+interface CarDesign{
+    //outline of car
+    void startUp();
+    void accelerate();
+}
+
+public class InterfaceMain implements CarDesign {
+
+    public static void main(String[] args){
+        String model = "SUV";
+        String color = "black";
+        String brand = "Audi";
+        System.out.println(model + "," + color + "," + brand);
+        InterfaceMain car = new InterfaceMain();
+        car.startUp();
+        car.accelerate();
+    }
+    // implementation of interface
+    @Override
+    public void startUp() {
+        System.out.println("Started up and ready to race!");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("Race begins....");
+    }
+}
